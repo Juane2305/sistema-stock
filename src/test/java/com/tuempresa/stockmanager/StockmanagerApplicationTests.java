fbfee0324bf780e321a04e1a5e90c45c6683f9e7
IgnacioFares <ignaciofares7@gmail.com>
2025-04-21 18:53:53 -0300

    Se crearon los paquetes

---
+++ src/test/java/com/tuempresa/stockmanager/StockmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuempresa.stockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
